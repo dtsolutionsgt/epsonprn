1e71dc16206157c4a1db45208a2249c30056a7b1
ejcalderongt <ejcalderon02@yahoo.com>
2022-01-05 12:19:00 -0600

    //#EJC20220105: Close the file.

--- app/src/main/java/com/dts/epsonprint/MainActivity.java
+++ app/src/main/java/com/dts/epsonprint/MainActivity.java
@@ -37,6 +37,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -223,11 +224,19 @@ public class MainActivity extends Activity implements  ReceiveListener {
         }
 
         try {
+
             mPrinter.addFeedLine(1);
             textData.delete(0, textData.length());
 
             while ((ss = dfile.readLine()) != null) {
                 textData.append(ss).append("\n");
+            }
+
+            //#EJC20220105: Close the file.
+            try {
+                dfile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
             copies--;
